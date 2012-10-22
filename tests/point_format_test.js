bdda657648348f3a4433a26cb3165380d200053c
Admiral Potato <admiral@nuclearpixel.com>
2012-10-22 01:58:57 -0600

    Building some more elaborate point format performance tests

--- tests/point_format_test.js
+++ tests/point_format_test.js
@@ -6,151 +6,297 @@ var timer = {
 			stop: false,
 			diff: false
 		};
-		rec = this.q[string];
-		var now = new Date();
+		var rec = this.q[string],
+			now = new Date();
 		rec.start = now.getTime();
-		console.log('starting ' + string);
+		document.body.innerHTML += 'starting ' + string + '<br>';
 	},
 	stop:function(string){
-		rec = this.q[string];
-		var now = new Date();
+		var rec = this.q[string],
+			now = new Date();
 		rec.stop = now.getTime();
 		rec.diff = rec.stop - rec.start;
-		console.log('ending ' + string, rec.diff);
+		return rec.diff;
 	}
 };
 
-var operationCount = 1000000;
-
-window.onload = function(){
-
-var arrayPointList = [];
-
-timer.start('Create array');
-for(var i = 0; i < operationCount; i+=1){
-	arrayPointList.push([
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]);
-}
-timer.stop('Create array');
-
-var obPointList = [];
-
-
-timer.start('Create object');
-for(var i = 0; i < operationCount; i+=1){
-	obPointList.push(new Vec3([
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]));
-}
-timer.stop('Create object');
-
-
-var getP3Scaled = function(p3,scale){
-	//return p3;
-	return [p3[0]*scale[0], p3[1]*scale[1], p3[2]*scale[2]];;
-}
-
-timer.start('Scale array');
-var transformedArrayList = [];
-for(var i = 0; i < operationCount; i+=1){
-	transformedArrayList.push(getP3Scaled(arrayPointList[i],[
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]));
-}
-timer.stop('Scale array');
-
-
-timer.start('Scale object');
-var transformedObList = [];
-for(var i = 0; i < operationCount; i+=1){
-	transformedObList.push(obPointList[i].clone().mul([
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]));
-}
-timer.stop('Scale object');
-
-
-
-
-
+var totalRotationCalculations = 0;
 var rotatePoint = function(x,y,rad){
-	var length = Math.sqrt((x * x) + (y * y));
-	var currentRad = Math.atan2(x,y);
+	var length = Math.sqrt((x * x) + (y * y)),
+		currentRad = Math.atan2(x,y);
 	x = Math.sin(currentRad - rad) * length;
 	y = Math.cos(currentRad - rad) * length;
-	var output = [x,y];
-	return output;
-}
-var totalRotationCalculations = 0;
-var getP3Rotated = function(p3,rot,order){
-	//return p3;
-	var order = order || [0,1,2];
-	var x = p3[0], y = p3[1], z = p3[2];
-	var xr = rot[0], yr = rot[1], zr = rot[2];
-	//Alright, here's something interesting.
-	//The order you rotate the dimentions is IMPORTANT to rotation animation!
-	//Here's my quick, no math approach to applying that.
-	for(var r = 0; r < order.length; r += 1){
+	totalRotationCalculations += 1;
+	return [x,y];
+};
+//--start array styled functions--
+var arrayClone = function(a){
+	return [a[0], a[1], a[2]];
+};
+var arrayAdd = function(a,b){
+	a[0] += b[0];
+	a[1] += b[1];
+	a[2] += b[2];
+};
+var arraySub = function(a,b){
+	a[0] -= b[0];
+	a[1] -= b[1];
+	a[2] -= b[2];
+};
+var arrayMul = function(a,n){
+	a[0] *= n;
+	a[1] *= n;
+	a[2] *= n;
+};
+var arrayDiv = function(a,n){
+	a[0] /= n;
+	a[1] /= n;
+	a[2] /= n;
+};
+var arrayScale = function(a,b){
+	a[0] *= b[0];
+	a[1] *= b[1];
+	a[2] *= b[2];
+};
+var arrayRotate = function(a,rot,order){
+	var x = a[0], y = a[1], z = a[2], r;
+	order = order || [0,1,2];
+	for(r = 0; r < order.length; r += 1){
 		if(order[r] === 0){
 			//x...
-			if(xr !== 0){
-				var zy = rotatePoint(z,y,xr);
+			if(rot[0] !== 0){
+				var zy = rotatePoint(z,y,rot[0]);
 				z = zy[0];
 				y = zy[1];
-				totalRotationCalculations += 1;
 			}
 		}else if(order[r] === 1){
 			//y...
-			if(yr !== 0){
-				var xz = rotatePoint(x,z,yr);
+			if(rot[1] !== 0){
+				var xz = rotatePoint(x,z,rot[1]);
 				x = xz[0];
 				z = xz[1];
-				totalRotationCalculations += 1;
 			}
 		}else if(order[r] === 2){
 			//z...
-			if(zr !== 0){
-				var xy = rotatePoint(x,y,zr);
+			if(rot[2] !== 0){
+				var xy = rotatePoint(x,y,rot[2]);
 				x = xy[0];
 				y = xy[1];
-				totalRotationCalculations += 1;
 			}
 		}else{
 			throw 'up';
 		}
 	}
-	return [x,y,z];
-}
-
-
-timer.start('Rotate array');
-for(var i = 0; i < operationCount; i+=1){
-	transformedArrayList[i] = getP3Rotated(transformedArrayList[i],[
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]);
-}
-timer.stop('Rotate array');
-
-
-timer.start('Rotate object');
-for(var i = 0; i < operationCount; i+=1){
-	transformedObList[i].rotate([
-		(Math.random() * 100),
-		(Math.random() * 100),
-		(Math.random() * 100)
-	]);
-}
-timer.stop('Rotate object');
-
+	a[0] = x;
+	a[1] = y;
+	a[2] = z;
+	return a;
 };
+//--end array styled functions--
+
+//--start object styled functions--
+var objectClone = function(a){
+	return {x: a.x, y: a.y, z: a.z};
+};
+var objectAdd = function(a,b){
+	a.x += b.x;
+	a.y += b.y;
+	a.z += b.z;
+};
+var objectSub = function(a,b){
+	a.x -= b.x;
+	a.y -= b.y;
+	a.z -= b.z;
+};
+var objectScale = function(a,b){
+	a.x *= b.x;
+	a.y *= b.y;
+	a.z *= b.z;
+};
+var objectMul = function(a,n){
+	a.x *= n;
+	a.y *= n;
+	a.z *= n;
+};
+var objectDiv = function(a,n){
+	a.x /= n;
+	a.y /= n;
+	a.z /= n;
+};
+var objectRotate = function(a,rot,order){
+	var x = a.x, y = a.y, z = a.z, r;
+	order = order || [0,1,2];
+	for(r = 0; r < order.length; r += 1){
+		if(order[r] === 0){
+			//x...
+			if(rot.x !== 0){
+				var zy = rotatePoint(z,y,rot.x);
+				z = zy[0];
+				y = zy[1];
+			}
+		}else if(order[r] === 1){
+			//y...
+			if(rot.y !== 0){
+				var xz = rotatePoint(x,z,rot.y);
+				x = xz[0];
+				z = xz[1];
+			}
+		}else if(order[r] === 2){
+			//z...
+			if(rot.z !== 0){
+				var xy = rotatePoint(x,y,rot.z);
+				x = xy[0];
+				y = xy[1];
+			}
+		}else{
+			throw 'up';
+		}
+	}
+	a.x = x;
+	a.y = y;
+	a.z = z;
+	return a;
+};
+//--end array styled functions--
+
+
+var approaches = {
+	array: {
+		create: function(input, output, key) {
+			input.push([
+				(Math.random() * 100),
+				(Math.random() * 100),
+				(Math.random() * 100)
+			]);
+		},
+		clone: function(input, output, key) {
+			output.push(arrayClone(input[key]));
+		},
+		mul: function(input, output, key) {
+			arrayMul(output[key], Math.random() * 100);
+		},
+		scale: function(input, output, key) {
+			arrayScale(output[key], [
+				(Math.random() * 100),
+				(Math.random() * 100),
+				(Math.random() * 100)
+			]);
+		},
+		rotate: function(input, output, key) {
+			arrayRotate(output[key], [
+				(Math.random() * 100),
+				(Math.random() * 100),
+				(Math.random() * 100)
+			]);
+		}
+	},
+	objectLiteral: {
+		create: function(input, output, key) {
+			input.push({
+				x: (Math.random() * 100),
+				y: (Math.random() * 100),
+				z: (Math.random() * 100)
+			});
+		},
+		clone: function(input, output, key) {
+			output.push(objectClone(input[key]));
+		},
+		mul: function(input, output, key) {
+			objectMul(output[key], Math.random() * 100);
+		},
+		scale: function(input, output, key) {
+			objectScale(output[key], {
+				x: (Math.random() * 100),
+				y: (Math.random() * 100),
+				z: (Math.random() * 100)
+			});
+		},
+		rotate: function(input, output, key) {
+			objectRotate(output[key], {
+				x: (Math.random() * 100),
+				y: (Math.random() * 100),
+				z: (Math.random() * 100)
+			});
+		}
+	},
+	vec3: {
+		create: function(input, output, key) {
+			input.push(new Vec3(
+				(Math.random() * 100),
+				(Math.random() * 100),
+				(Math.random() * 100)
+			));
+		},
+		clone: function(input, output, key) {
+			output.push(input[key].clone());
+		},
+		mul: function(input, output, key) {
+			output[key].mul(Math.random() * 100);
+		},
+		scale: function(input, output, key) {
+			output[key].scale(
+				(Math.random() * 100),
+				(Math.random() * 100),
+				(Math.random() * 100)
+			);
+		},
+		rotate: function(input, output, key) {
+			output[key].rotate(
+				[
+					(Math.random() * 100),
+					(Math.random() * 100),
+					(Math.random() * 100)
+				]
+			);
+		}
+	}
+};
+
+var operationCount = 500000;
+var speedTest = function(taskName, task, input, output){
+	var i;
+	timer.start(taskName);
+	for(i = 0; i < operationCount; i += 1){
+		task(input, output, i);
+	}
+	return timer.stop(taskName);
+};
+
+var arrayList = [],
+	arrayListTransformed = [],
+	objectLiteralList = [],
+	objectLiteralListTransformed = [],
+	vec3List = [],
+	vec3ListTransformed = [];
+
+alert('Be patient, this is going to take a minute.');
+var approach, task, result, table = ['All operations completed ', operationCount, ' times. Smaller numbers are better.<br><table border="1">'], tableHeader = [], tableRow = [], firstRowComplete = false;
+for(approachName in approaches){
+	if(approaches.hasOwnProperty(approachName)){
+		approach = approaches[approachName];
+		for(taskName in approach){
+			if(approach.hasOwnProperty(taskName)){
+				task = approach[taskName];
+				console.log(approachName + ' ' + taskName);
+				if(!firstRowComplete){
+					tableHeader.push('<th>', taskName, '</th>');
+				}
+				result = speedTest(
+					approachName + ' ' + taskName,
+					task,
+					window[approachName + 'List'],
+					window[approachName + 'ListTransformed']
+				);
+				tableRow.push('<td>', result, '</td>');
+			}
+		}
+		if(!firstRowComplete){
+			table.push('<thead><tr><th>Type</th>', tableHeader.join(''), '</tr></thead><tbody>');
+			firstRowComplete = true;
+		}
+		table.push('<tr><th>', approachName, '</th>', tableRow.join(''), '</tr>');
+		tableRow = [];
+	}
+}
+table.push('</tbody></table>');
+document.body.innerHTML += table.join('');
--- tests/vec3min.js
+++ tests/vec3min.js
@@ -1,28 +1,43 @@
-var Vec3 = function(p){
-	var v = p || [0,0,0];
-	if(this===window){throw 'This is a constructor method. You must create "new" instances of it.';}
-	this.x = v[0];
-	this.y = v[1];
-	this.z = v[2];
-	//console.log('creating a new Vec2');
+var Vec3 = function(args){
+	var type = 'Vec3', v;
+	if(this.type !== type){throw 'You must use the `new` keyword when invoking the ' + type + ' constructor.';}
+	v = this.parseArgs(arguments);
+	this.x = v.x;
+	this.y = v.y;
+	this.z = v.z;
 	return this;
 };
 
 Vec3.prototype = {
-	sep:'x',
+	type: 'Vec3',
+	parseArgs: function(){
+		var output;
+		if(arguments.length === 3) { //vanilla
+			output = {x: arguments[0],y: arguments[1],z: arguments[2]};
+		} else if(arguments[0].length !== undefined && arguments[0].length > 2) { //is array
+			output = {x: arguments[0][0],y: arguments[0][1],z: arguments[0][2]};
+		}else if(arguments[0].x !== undefined && arguments[0].y !== undefined && arguments[0].z !== undefined) { //is object
+			output = arguments[0];
+		} else {
+			output = {x: 0, y: 0, z: 0};
+		}
+		return output;
+	},
 	clone:function(){
 		return new Vec3(this);
 	},
-	add:function(v){
-		this.x += v[0];
-		this.y += v[1];
-		this.z += v[2];
+	add:function(){
+		var v = this.parseArgs(arguments);
+		this.x += v.x;
+		this.y += v.y;
+		this.z += v.z;
 		return this;
 	},
-	sub:function(v){
-		this.x -= v[0];
-		this.y -= v[1];
-		this.z -= v[2];
+	sub:function(){
+		var v = this.parseArgs(arguments);
+		this.x -= v.x;
+		this.y -= v.y;
+		this.z -= v.z;
 		return this;
 	},
 	mul:function(num){
@@ -40,9 +55,9 @@ Vec3.prototype = {
 		return this;
 	},
 	scale:function(v){
-		this.x *= v[0];
-		this.y *= v[1];
-		this.z *= v[2];
+		this.x *= v.x;
+		this.y *= v.y;
+		this.z *= v.z;
 		return this;
 	},
 	length:function(){
@@ -51,49 +66,41 @@ Vec3.prototype = {
 	sqlen:function(){
 		return ((this.x*this.x) + (this.y*this.y) + (this.z*this.z));
 	},
-	angle2d:function(x,y){
-		return Math.atan2(x,y);
-	},
 	length2d:function(x,y){
 		return Math.sqrt((x*x) + (y*y));
 	},
 	rotate2d:function(x,y,r){
-		x = parseFloat(x);
-		y = parseFloat(y);
-		//the r stands for radians
-		r = parseFloat(r);
 		//current Radians, current Length
-		var cR = this.angle2d(x,y);
-		var cL = this.length2d(x,y);
+		var cR = Math.atan2(y,x),
+			cL = this.length2d(x,y);
 		var newX = Math.sin(cR + r) * cL;
 		var newY = Math.cos(cR + r) * cL;
 		return [newX,newY];
 	},
 	rotate:function(rot,order){
-		var order = order || [0,1,2];
-		var xr = rot[0], yr = rot[1], zr = rot[2];
+		order = order || [0,1,2];
 		//Alright, here's something interesting.
-		//The order you rotate the dimentions is IMPORTANT to rotation animation!
+		//The order you rotate the dimensions is IMPORTANT to rotation animation!
 		//Here's my quick, no math approach to applying that.
 		for(var r = 0; r < order.length; r += 1){
 			if(order[r] === 0){
 				//x...
-				if(xr !== 0){
-					var zy = this.rotate2d(this.z,this.y,xr);
+				if(rot[0] !== 0){
+					var zy = this.rotate2d(this.z,this.y,rot[0]);
 					this.z = zy[0];
 					this.y = zy[1];
 				}
 			}else if(order[r] === 1){
 				//y...
-				if(yr !== 0){
-					var xz = this.rotate2d(this.x,this.z,yr);
+				if(rot[1] !== 0){
+					var xz = this.rotate2d(this.x,this.z,rot[1]);
 					this.x = xz[0];
 					this.z = xz[1];
 				}
 			}else if(order[r] === 2){
 				//z...
-				if(zr !== 0){
-					var xy = this.rotate2d(this.x,this.y,zr);
+				if(rot[2] !== 0){
+					var xy = this.rotate2d(this.x,this.y,rot[2]);
 					this.x = xy[0];
 					this.y = xy[1];
 				}
@@ -103,10 +110,11 @@ Vec3.prototype = {
 		}
 		return this;
 	},
-	set:function(v){
-		this.x = v[0];
-		this.y = v[1];
-		this.z = v[2];
+	set:function(){
+		var v = this.parseArgs(arguments);
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
 		return this;
 	},
 	toString:function(){
@@ -115,11 +123,8 @@ Vec3.prototype = {
 	toUrlString:function(){
 		return this.x+this.sep+this.y+this.sep+this.z;
 	},
-	clone:function(){
-		return new Vec3(this);
-	},
 	normalize:function(){
 		this.div(this.length());
-		return t;
+		return this;
 	}
 };
