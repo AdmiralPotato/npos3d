375d6008f8061785ab59f780237758ca14edb7d2
Admiral Potato <admiral@nuclearpixel.com>
2012-11-25 19:05:16 -0700

    Performing a little cleanup on n.Geom.Circle, adding `axies` argument

--- src/geom.circle.js
+++ src/geom.circle.js
@@ -8,34 +8,38 @@ NPos3d.Geom.Circle = function(args){
 	t.points = [];
 	t.lines = [];
 	t.radius = args.radius || 20;
+	t.axies = args.axies || [0,1,2];
 	t.formCircle();
 	return t;
-}
-
+};
 NPos3d.Geom.Circle.prototype = {
 	type: 'Circle',
 	formCircle: function(){
-		var t = this;
+		var t = this,
+			m = NPos3d.Maths,
+			slice = m.tau / t.segments,
+			i, point, angle;
 		t.points = [];
 		t.lines = [];
-		var rad = tau / t.segments;
-		var z = 0;
-		for(var point = 0; point < t.segments; point += 1){
-			var x = Math.sin((rad*point) + t.offset) * t.radius;
-			var y = Math.cos((rad*point) + t.offset) * t.radius;
-			t.points.push([x,y,z]);
-
+		for(i = 0; i < t.segments; i += 1){
+			point = [];
+			angle = (slice * i) + t.offset;
+			//relative x
+			point[t.axies[0]] = m.cos(angle) * t.radius;
+			//relative y
+			point[t.axies[1]] = m.sin(angle) * t.radius;
+			//relative z
+			point[t.axies[2]] = 0;
+			t.points.push(point);
 			//creates the line between the current point and the previous point
-			if(point > 0 && point <= t.segments){
-				t.lines.push([point -1 ,point]); //,'#0f0'
-
+			if(i > 0 && i <= t.segments){
+				t.lines.push([i -1, i]); //,'#0f0'
 				//closes the gap between the first point and the ending point
-				if(point === t.segments -1){
-					t.lines.push([point, 0]); //,'#f0f'
+				if(i === t.segments -1){
+					t.lines.push([i, 0]); //,'#f0f'
 				}
-
 			}
 		}
 		return t;
 	}
-}
+};
